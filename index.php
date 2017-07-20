a7d543373bca13f0ddba1e9be4f5619614baff05
YuriyMironov <jura96_@mail.ru>
2017-07-20 12:16:41 +0300

    2 commit

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-php.phpinfo(); 
-?> 
+<form action="" method="post">
+ <p>Даун: <input type="text" name="sub" /></p>
+ <p><input type="submit" /></p>
+</form>
+<?php 
+if (isset($_POST['sub'])){
+echo $_POST['sub'].'pr08.iktiscloud.ru'; 
+}
+?>
