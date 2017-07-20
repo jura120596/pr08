430c96a9c01787c9df8d364c6a614e1e13b8de7b
YuriyMironov <jura96_@mail.ru>
2017-07-20 13:35:56 +0300

    13

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
 if (isset($_POST['sub'])){
 	echo $_POST['sub'].'pr08.iktiscloud.ru'; 
 	if (!file_exists($_POST['sub'])) {
-		mkdir($_POST['sub'] , 0777, true);
+			mkdir($_POST['sub'] , 0777, true);
 		$file = 'index.php2';
 		$newfile = $_POST['sub'].'/index.php';
 		if (!copy($file, $newfile)) {
-		echo "не удалось скопировать $file...\n";
+			echo "не удалось скопировать $file...\n";
+		}
 	}else{
 		echo 'sub domain exists';
 	}
-}
+
 }
 ?>
