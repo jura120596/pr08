e32d4d7a2187612a70debb0171233be9d91d8587
YuriyMironov <jura96_@mail.ru>
2017-07-20 13:20:48 +0300

    9

--- index.php
+++ index.php
@@ -3,18 +3,15 @@
  <p><input type="submit" /></p>
 </form>
 <?php 
-	if (isset($_POST['sub'])){
-		$sub = $_POST['sub']
-		echo $sub.'pr08.iktiscloud.ru'; 
-		if (!file_exists($sub)) {
-			mkdir($sub , 0777, true);
-		}else{
-			echo 'sub domain exists';
-		}
-		$file = 'index.php2';
-		$newfile = $sub.'/index.php';
-		if (!copy($file, $newfile)) {
-			echo "can't create folder for new subdomain";
-		}
+if (isset($_POST['sub'])){
+	$sub = $_POST['sub']
+	echo $sub.'pr08.iktiscloud.ru'; 
+	if (!file_exists($sub)) {
+		mkdir($sub , 0777, true);
+	}else{
+		echo 'sub domain exists';
 	}
+	$file = 'index.php2';
+	$newfile = $sub.'/index.php';
+}
 ?>
