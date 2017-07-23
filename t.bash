de9ad7367a125274cd3d6b6ea3bf1ed1e7a1e28c
YuriyMironov <jura96_@mail.ru>
2017-07-23 19:40:04 +0300

    21

--- t.bash
+++ t.bash
@@ -1,2 +1,14 @@
 #!/bin/bash
-mkdir hellophp
+name=$1
+declare name=$1
+declare cname="$name.azurewebsites.net"
+declare mhostname="$name.pr08.itiscloud.ru"
+az account show 1> /dev/null
+if [ $? != 0 ];
+then
+az login
+fi
+az account set --subscription "89da4d0c-7daa-4d45-b9cc-ab6987be1e41"
+az webapp create -g pr2017 -p ServicePlane0ceef23-bd0a -n $name --runtime "PHP|7.0" --deployment-source-url https://github.com/jura120596/pr08
+az network dns record-set cname set-record -n $name -c $cname --resource-group pr2017 --zone-name pr08.itiscloud.ru
+az webapp config hostname add --hostname $mhostname -g pr2017 --webapp-name $name
