fb024647af32c45c68435817122a82e1e5217a2e
YuriyMironov <jura96_@mail.ru>
2017-07-23 20:42:01 +0300

    30

--- index.php
+++ index.php
@@ -5,17 +5,7 @@
 <?php 
 if (isset($_POST['sub'])){
 	echo '<a href="http://'.$_POST['sub'].'.pr08.itiscloud.ru">http://'.$_POST['sub'].'.pr08.itiscloud.ru</a>'; 
-	//if (!file_exists($_POST['sub'])) {
-			// mkdir($_POST['sub'] , 0777, true);
-		//$file = 'index.php2';
-		//$newfile = $_POST['sub'].'/index.php';
-		//if (!copy($file, $newfile)) {
-			//echo "не удалось скопировать $file...\n";
-		//}
 		shell_exec('bash date > lastcreate.txt');
-		shell_exec('bash ./t.bash'.$_POST['sub'].'');
-	//}else{
-		//echo 'sub domain exists';
-	//}
+		shell_exec('bash ./t.bash'.$_POST['sub'].' && echo test');
 }
 ?>
